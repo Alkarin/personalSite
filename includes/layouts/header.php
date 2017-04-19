99525730759b2868b45a7981dc69942f55222189
Alkarin <avaughan11@gmail.com>
2017-04-18 23:56:07 -0700

    initial layout integration

---
+++ includes/layouts/header.php
@@ -0,0 +1,29 @@
+<?php
+  if(!isset($layout_context)){
+    $layout_context = "public";
+  }
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Alexander Vaughan</title>
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/master.css" media="all" title="no title" type="text/css" charset="utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+  </head>
+  <body>
+    <div id="cover" class="parallax">
+      <div class="caption">
+          <p class="greybox">Alexander Vaughan</p>
+          <p class="greybox">Software Developer</p>
+      </div>
+    </div>
+    <div id="navigation">
+      <a href="#signin">Sign in</a>
+      <a href="#about">About</a>
+      <a href="#contact">Contact</a>
+      <a href="#news">News</a>
+      <a class="active" href="#home">Home</a>
+    </div>
---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php include("../includes/layouts/header.php"); ?>
+
+
+<div id="main">
+  <div id="tableOfContent">
+    <ul>
+      <li><h2>test</h2></li>
+    </ul>
+    <br>
+    <?php //echo public_navigation($current_subject,$current_page); ?>
+    <br>
+  </div>
+  <div id="page">
+    <h1>Hello World</h1>
+    <p>lorem ipsum </p>
+  </div>
+</div>
+<?php include("../includes/layouts/footer.php"); ?>
