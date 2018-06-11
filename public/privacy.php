eb61865252e9d0a28534598c42e5469c97f3d27a
AlexanderV <avaughan@barefootsolutions.com>
2018-06-11 13:36:48 -0700

    Legalise initial template

--- public/privacy.php
+++ public/privacy.php
@@ -1,8 +1,9 @@
 <?php include("../includes/layouts/header.php"); ?>
 
     <div id="main">
-        <div>Icons made by Freepik from www.flaticon.com </div>
-        <a href="https://www.flaticon.com/authors/freepik">Check out Freepik here</a>
+        <p>Insert legalise here</p>
+        <a href="https://www.flaticon.com/authors/freepik" target="_blank">Icons made by Freepik from www.flaticon.com </a>
+        <a href="http://jackonthe.net/css3animateit/" target="_blank">CSS3 Animate It</a>
     </div>
 
 <?php include("../includes/layouts/footer.php"); ?>
