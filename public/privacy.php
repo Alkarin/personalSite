32e07cfdbf6f57520fc60019a5174b5bc0996bed
AlexanderV <avaughan@barefootsolutions.com>
2018-06-11 13:37:48 -0700

    Add spacing

--- public/privacy.php
+++ public/privacy.php
@@ -3,6 +3,7 @@
     <div id="main">
         <p>Insert legalise here</p>
         <a href="https://www.flaticon.com/authors/freepik" target="_blank">Icons made by Freepik from www.flaticon.com </a>
+        <br>
         <a href="http://jackonthe.net/css3animateit/" target="_blank">CSS3 Animate It</a>
     </div>
 
